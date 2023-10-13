03cc3e540e4f2140abee8fec6376ca8509c6f7f7
Err-G <err-g@users.noreply.github.com>
2023-10-13 22:50:46 +0100

    shell script yay

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Specify the files and directories to be installed
+files=("tmux.conf" "vimrc" "zshrc")
+directories=("vim" "nvim")
+
+# Backup existing files
+backup_dir="$HOME/dotfiles_backup"
+mkdir -p "$backup_dir"
+
+# Function to backup and install files
+backup_and_install() {
+    local file_or_dir="$1"
+    if [ -e "$HOME/.$file_or_dir" ]; then
+        mv "$HOME/.$file_or_dir" "$backup_dir/"
+        echo "Backup: .$file_or_dir -> $backup_dir/$file_or_dir"
+    fi
+    cp -r "$file_or_dir" "$HOME/.$file_or_dir"
+    echo "Installed: .$file_or_dir -> $HOME/.$file_or_dir"
+}
+
+# Install files
+for file in "${files[@]}"; do
+    backup_and_install "$file"
+done
+
+# Install directories
+for directory in "${directories[@]}"; do
+    backup_and_install "$directory"
+done
+
+# Additional installation steps, if needed
+# ...
+
+echo "Installation complete."
+
